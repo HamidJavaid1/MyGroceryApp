786d68408e9ae2b7aad221b5e819f35884264a9e
hamidjavaid1 <hamidjavaid1@gmail.com>
2026-06-03 11:53:10 +0500

    Disable all middleware except WhiteNoise to isolate 400 error

--- backend/bazarlink/settings.py
+++ backend/bazarlink/settings.py
@@ -60,15 +60,15 @@ JAZZCASH_INITIATE_URL = config("JAZZCASH_INITIATE_URL", default="")
 JAZZCASH_CONFIRM_URL = config("JAZZCASH_CONFIRM_URL", default="")
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",  # Disabled temporarily
     # "django.middleware.security.SecurityMiddleware",  # Disabled temporarily
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    # "django.contrib.sessions.middleware.SessionMiddleware",  # Disabled temporarily
     # "django.middleware.common.CommonMiddleware",  # Disabled temporarily
     # "django.middleware.csrf.CsrfViewMiddleware",  # Disabled for JWT API
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # "django.contrib.auth.middleware.AuthenticationMiddleware",  # Disabled temporarily
+    # "django.contrib.messages.middleware.MessageMiddleware",  # Disabled temporarily
+    # "django.middleware.clickjacking.XFrameOptionsMiddleware",  # Disabled temporarily
 ]
 
 ROOT_URLCONF = "bazarlink.urls"
